8abf9030de53b2be209f6957e71790f531ce3967
Nasuf Mutlu <nasufmutlu@hotmail.com>
2021-01-23 19:50:25 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<I,P>
+    {
+        I[] id;
+        I[] tempId;
+        P[] name;
+        P[] plaka;
+
+        public Dictionary()
+        {
+            id = new I[0];
+            name = new P[0];
+        }
+
+        public void Add(I key,P value)
+        {
+            tempId = id;
+            plaka = name;
+
+            id = new I[id.Length + 1];
+            name = new P[name.Length + 1];
+
+            for (int i = 0; i < plaka.Length; i++)
+            {
+                plaka[i] = name[i];
+                id[i] = tempId[i];
+            }
+            name[name.Length - 1] = value;
+            id[id.Length - 1] = key;
+
+            Console.WriteLine("Plaka: "+key+" /İl: "+value+" Eklendi.");
+        }
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> iller = new Dictionary<int, string>();
+            iller.Add(35, "İzmir");
+            iller.Add(06, "Ankara");
+            iller.Add(01, "Adana");
+        }
+    }
+}
